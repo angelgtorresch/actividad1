5e2e7538ed00043bad3c84c1b14da8ad634978aa
Angel Torres <angelgtorresch@outlook.com>
2024-04-02 15:07:19 -0600

    first commit

---
+++ actividad1.js
@@ -0,0 +1,21 @@
+// introducir fecha
+//localstore
+
+
+const numerOrdenes = numerOrdenes();
+const diaDelMes = diaDelMes(getDate);
+
+
+if (diaDelMes / 2 == 0){
+    console.log ("dia es par")
+}
+else{
+    console.log ("dia no es par")
+}
+
+if (numerOrdenes = i+1){
+    console.log ("supera el monto preevisto")
+}
+else{
+    console.log ("no supero lo preevisto")
+}
