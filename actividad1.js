0c53ab1615d0a996eb63ce9d6afb8eee27695eb4
Angel Torres <angelgtorresch@outlook.com>
2024-04-03 13:49:05 -0600

    actualizacion

---
+++ actividad1.js
@@ -0,0 +1,21 @@
+let fechaActual = new Date();
+    console.log (fechaActual.getDate());
+//localstore
+
+let numerOrdenes = 15
+
+
+
+if (fechaActual % 2 ){
+    console.log ("dia es par")
+}
+else{
+    console.log ("dia no es par")
+}
+
+if (numerOrdenes = i+1){
+    console.log ("supera el monto preevisto")
+}
+else{
+    console.log ("no supero lo preevisto")
+}
